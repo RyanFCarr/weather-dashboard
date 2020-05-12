e6d2cab3190cbd69fe942912f9518c5e873a2483
Ryan Carr <ryanfcarr@gmail.com>
2020-05-11 23:17:18 -0400

    Implemented search function

---
+++ assets/script.js
@@ -0,0 +1,22 @@
+var city = "";
+
+$( "#btnSearch" ).click(function() {
+    city = $("#form-search").val();
+getWeather()
+});
+
+function getWeather(){
+
+    var apiKey = "6a13c0be5285df65c709a8ea0f1f8378";
+  
+    var URI = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    $.ajax({
+      url: URI,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response)
+      
+    });
+  }
+  
