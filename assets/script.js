41332d4e72851eb472a3f60cc5e8b39255fe9e0f
Ryan Carr <ryanfcarr@gmail.com>
2020-05-12 22:04:59 -0400

    functionality and data for all API calls implemented

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,66 @@
 
-var city = "";
+var currentCity = "";
+var searchHistory = JSON.parse(localStorage.getItem("location")) || [];
+var apiKey = "6a13c0be5285df65c709a8ea0f1f8378";
+var coords = {};
+var uvIndex = 0;
 
-//Get & process city.
-$( "#btnSearch" ).click(function() {
-    city = $("#form-search").val();
-    //Local Storage
-    localStorage.setItem("location", city);
-getWeather()
+//click listner
+$( "#search-form" ).submit(function(event) {
+  currentCity = $("#form-search").val();
+  getCurrent();
+  event.preventDefault();
 });
 
-function getWeather(){
+function getForecast(){
+  var URI = `http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=${currentCity}&appid=${apiKey}`;
+  $.ajax({
+    url: URI,
+    method: "GET"
+  }).then(function(response) {
 
-    var apiKey = "6a13c0be5285df65c709a8ea0f1f8378";
-    var URI = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    var weeklyForecast = $("#weekly-forecast")
+    render();
+  });
+}
 
-    $.ajax({
-      url: URI,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response)
-      
-    });
-  }
+function getCurrent(){
+  var URI = `http://api.openweathermap.org/data/2.5/weather?units=imperial&q=${currentCity}&appid=${apiKey}`;
+  var currentForecast = $("#current-forecast")
+
+  $.ajax({
+    url: URI,
+    method: "GET"
+  }).then(function(response) {
+    searchHistory.push(currentCity); 
+    localStorage.setItem("location", JSON.stringify(searchHistory));
+    coords = response.coord;
+    getUV();
+  });
+}
+
+function getUV(){
+  var URI = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${coords.lat}&lon=${coords.lon}`;
+  $.ajax({
+    url: URI,
+    method: "GET"
+  }).then(function(response){
+    uvIndex = response.value;
+    getForecast();
+  });   
+}
+
+render();
+function render(){
+  var historyEl = $("#history");
+  historyEl.html("");
+  var cities = JSON.parse(localStorage.getItem("location"))
+  $.each(cities, function(i, city){
+      var li = $("<li>").text(city).addClass("list-group-item list-group-item-action");
+      historyEl.append(li)
+  })
+}
+
   
 
 
